ef7a5ec4df628bc9ee3c93e5ba0df781f9e0410f
Wesley <Gamer@DESKTOP-PQIIC82>
2021-01-16 03:57:42 -0500

    01/16/2021 Commit

---
+++ src/webPages/webPageLinks/ContentMaster.js
@@ -0,0 +1,49 @@
+/**
+ * Content on every informational webpage is imported from this JS file.
+ * NOTE: Each object can carry up to five values, otherwise the string content may not appear on the app. Do NOT
+ * assign a variable to an array of objects.
+ * It is required to import/export images using ES6 modules.
+ * 
+ * All web page components are located in ./src/webPages directory.
+ * Universal Page Content - Page content that can appear on any web page.
+ * Contact_Us Content - Page content that can appear on Contact_Us.js.
+ * Portfolio_Casework Content - Page content that can appear on Portfolio_Casework.js.
+ * Services Content - Page content that can appear on Services.js.
+ * Why_MBM Content - Page content that can appear on Why_MBM.js.
+ */
+
+ /**
+  * /////////////////////////////////////////////////// UNIVERSAL PAGE IMAGES //////////////////////////////////////////////////////////////////
+  */
+
+  import websiteMasterLogo from '../../images/logoImages/MBMDentalLogo_Year.png';
+
+ /**
+  * /////////////////////////////////////////////////// UNIVERSAL PAGE CONTENT /////////////////////////////////////////////////////////////////
+  */
+
+   const noImage = "Image has failed to load."; //Image failed to load message.
+
+  /**
+  * /////////////////////////////////////////////////// CONTACT_US PAGE CONTENT ////////////////////////////////////////////////////////////////
+  */
+
+  /**
+  * /////////////////////////////////////////////////// PORTFOLIO_CASEWORK PAGE CONTENT ////////////////////////////////////////////////////////
+  */
+
+  /**
+  * /////////////////////////////////////////////////// SERVICES PAGE CONTENT //////////////////////////////////////////////////////////////////
+  */
+
+  /**
+  * /////////////////////////////////////////////////// WHY_MBM PAGE CONTENT ///////////////////////////////////////////////////////////////////
+  */
+
+  export { 
+    websiteMasterLogo,
+    /////////////////////
+    noImage
+    /////////////////////
+  };
+  
--- src/webPages/webPageLinks/Header.js
+++ src/webPages/webPageLinks/Header.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { noImage, websiteMasterLogo } from '../../webPages/webPageLinks/ContentMaster.js';
+
 const Header = () => {
 
     return(
         <header className="p-3">
-            <Link to="/" className="bg-gray-200 p-3 text-center text-lg">
-                Why MBM
-            </Link>
-    
-            <Link to="/Services" className="bg-gray-200 p-3 text-center text-lg">
-                Services
-            </Link>
-    
-            <Link to="/Portfolio_Casework" className="bg-gray-200 p-3 text-center text-lg">
-                Portfolio Casework
-            </Link>
-    
-            <Link to="/Contact_Us" className="bg-gray-200 p-3 text-center text-lg">
-                Contact Us
-            </Link>
+            <nav className ="flex justify-left">
+                <img
+                    src={websiteMasterLogo}
+                    alt={noImage}
+                    className="h-64 w-64 focus:outline-none"
+                >
+                </img>
+                <Link to="/" className="bg-gray-200 p-3 text-center text-lg">
+                    Why MBM
+                </Link>
+            
+                <Link to="/Services" className="bg-gray-200 p-3 text-center text-lg">
+                    Services
+                </Link>
+            
+                <Link to="/Portfolio_Casework" className="bg-gray-200 p-3 text-center text-lg">
+                    Portfolio Casework
+                </Link>
+            
+                <Link to="/Contact_Us" className="bg-gray-200 p-3 text-center text-lg">
+                    Contact Us
+                </Link>
+            </nav>
         </header>
     );
 };
